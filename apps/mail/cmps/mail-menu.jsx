20ea231702a80ff0ac46f07fda946be83e544cdd
Eyal-Avni <67070652+Eyal-Avni@users.noreply.github.com>
2023-05-11 08:51:08 +0300

    improvments to mail menu UI

--- apps/mail/cmps/mail-menu.jsx
+++ apps/mail/cmps/mail-menu.jsx
@@ -18,20 +18,33 @@ export function MailMenu() {
             >
                 <img src="../../../assets/icons/compose.svg" alt="" />
             </button>
-            <div className="mail-categories">
-                <button className="mail-star">
-                    <img src="../../../assets/icons/star.png" alt="" />
-                </button>
-                <button className="mail-sent">
-                    <img src="../../../assets/icons/send.png" alt="" />
-                </button>
-                <button className="mail-draft">
-                    <img src="../../../assets/icons/draft.png" alt="" />
-                </button>
-                <button className="mail-trash">
-                    <img src="../../../assets/icons/delete.png" alt="" />
-                </button>
-            </div>
+            <ul className="mail-categories clean-list">
+                <li>
+                    <button className="mail-inbox">
+                        <img src="../../../assets/icons/inbox.png" alt="" />
+                    </button>
+                </li>
+                <li>
+                    <button className="mail-star">
+                        <img src="../../../assets/icons/star.png" alt="" />
+                    </button>
+                </li>
+                <li>
+                    <button className="mail-sent">
+                        <img src="../../../assets/icons/send.png" alt="" />
+                    </button>
+                </li>
+                <li>
+                    <button className="mail-draft">
+                        <img src="../../../assets/icons/draft.png" alt="" />
+                    </button>
+                </li>
+                <li>
+                    <button className="mail-trash">
+                        <img src="../../../assets/icons/delete.png" alt="" />
+                    </button>
+                </li>
+            </ul>
 
             {isCompose && <MailCompose />}
         </React.Fragment>
