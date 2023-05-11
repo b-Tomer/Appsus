65d5ab830a61d7f47070cd2afbaab340781672c9
T0mer007 <tomerbenaim1@gmail.com>
2023-05-12 01:27:45 +0300

    added nav routes

--- apps/keep/cmps/keep-header.jsx
+++ apps/keep/cmps/keep-header.jsx
@@ -10,24 +10,29 @@ export function KeepHeader() {
 
   return (
     <header className="keep-header full main-layout">
-        <section className="header-btns" >
-        <button className="btn btn-opt"><i className="fa-solid fa-circle-half-stroke"></i></button>
-        <button className="btn btn-opt"><i className="fa-solid fa-table"></i></button>
-        <button className="btn btn-opt"><i className="fa-solid fa-list"></i></button>
+      <div className="left-menu">
+        <button onClick={() => setMenuBtn(!menuBtn)} className="btn menu-btn" >
+          <i className="fa-solid fa-bars"></i>
+        </button>
+        <NavLink to="/keep" className="keeper-nav"><img className="icon" src="assets/img/keep-icon.png" alt="" /></NavLink>
+        
+        <span className="app-title">Keep</span>
+      </div>
+
+<div className="right-menu">
+      <section className="header-btns" >
         <button className="btn btn-opt"><i className="fa-solid fa-magnifying-glass"></i></button>
-        </section>
-
-      <h1>Keep</h1><img className="icon" src="assets/img/keep-icon.png" alt="" />
-      <nav className={`app-nav ${menuClass}`}>
-
-
-        {/* <NavLink onClick={() => setMenuBtn(false)} to="/" >Home</NavLink>
-        <NavLink onClick={() => setMenuBtn(false)} to="/about" >About</NavLink>
-      <NavLink onClick={() => setMenuBtn(false)} to="/book" >Books</NavLink> */}
-      </nav>
-      <button onClick={() => setMenuBtn(!menuBtn)} className="btn menu-btn" >
-        <i className="fa-solid fa-bars"></i>
-      </button>
+        <button className="btn btn-opt"><i className="fa-solid fa-list"></i></button>
+        <button className="btn btn-opt"><i className="fa-solid fa-circle-half-stroke"></i></button>
+        {/* <button className="btn btn-opt"><i className="fa-solid fa-table"></i></button> */}
+      </section>
+      <section className="nav-links">
+        <NavLink to="/book" className="bookk "><div className=""><i className="bookk-icon fa-solid fa-book-open"></i></div></NavLink>
+        <NavLink to="/mail" className="gmaill "><img className="gmaill-img animate__pulse" src="./assets/img/gmail-icon.png" alt="" /></NavLink>
+        <NavLink to="/" className="gmaill "><img className="gmaill-img animate__pulse" src="./assets/img/home-icon.png" alt="" /></NavLink>
+        
+      </section>
+</div>
 
     </header>
   )
