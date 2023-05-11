2998f811c12ed81796c335f0460e72219e9bdaf2
T0mer007 <tomerbenaim1@gmail.com>
2023-05-11 23:01:05 +0300

    home page

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -3,14 +3,11 @@ const { Link, NavLink } = ReactRouterDOM
 export function AppHeader() {
 
     return <header className="app-main-nav">
-        <Link to="/">
-            
-        </Link>
-        <nav>
-            <NavLink to="/"><img src="./assets/img/.jpg" alt="" /></NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail"><img src="./assets/img/gmail-icon.jpg" alt="" /></NavLink>
-            <NavLink to="/keep">Keep</NavLink>
+      
+        <nav className="nav-container">
+            <NavLink to="/"><div className="home-container"><i class="home-icon icon-img animate__pulse fa-solid fa-house-chimney"></i></div></NavLink>
+            <NavLink to="/mail"><img className="icon-img animate__pulse" src="./assets/img/gmail-icon.png" alt="" /></NavLink>
+            <NavLink to="/keep"><img className="icon-img animate__pulse" src="./assets/img/keep-icon.png" alt="" /></NavLink>
         </nav>
     </header>
 }
--- views/home.jsx
+++ views/home.jsx
@@ -1,8 +1,12 @@
 import { AppHeader } from '../cmps/app-header.jsx'
+import { About } from '../cmps/about.jsx'
+import { MainFooter } from '../cmps/main-footer.jsx'
 
 export function Home() {
     return <section className="home">
            <AppHeader />
+           {/* <About/> */}
+           <MainFooter/>
     </section>
 }
 
